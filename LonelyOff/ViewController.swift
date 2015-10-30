36cc3cf77a84740e7c7aa943c6eaa117eeb7dbc2
jabue <sdutyang@outlook.com>
2015-10-30 10:10:56 -0700

    Initial Commit

---
+++ LonelyOff/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LonelyOff
+//
+//  Created by EV Technologies Inc. on 2015-10-30.
+//  Copyright © 2015 Jabue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
